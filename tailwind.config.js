88eb9822a5c6f9430f4d2fd79eb0af6dc365fafb
Alan Comley <dev@comley.io>
2021-04-15 08:59:47 -0500

    css fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./www/**/*.html', './src/**/*.ts'],
+  purge: ['docs/**/*.html', 'src/**/*.ts'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
